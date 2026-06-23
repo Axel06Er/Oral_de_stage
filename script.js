d77ecba3fc9f8a0bdda481a5c9735ee3f5135947
Axel06Er <ernoult.axel@gmail.com>
2026-06-23 10:45:15 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,17 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const welcomeTitle = document.getElementById('welcome-title');
-    const liveClock = document.getElementById('live-clock');
-
-    function updateTime() {
-        const now = new Date();
-        const hour = now.getHours();
-        const minutes = now.getMinutes().toString().padStart(2, '0');
-        const seconds = now.getSeconds().toString().padStart(2, '0');
 
 
-        if (hour >= 18) {
-            welcomeTitle.textContent = "Bonsoir ! Bienvenue sur mon tableau de compétences";
-        } else {
-            welcomeTitle.textContent = "Bonjour ! Bienvenue sur mon tableau de compétences";
-        }
-
-  
-        liveClock.textContent = `Il est actuellement ${hour}h${minutes}:${seconds}`;
-    }
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
 
 
-    updateTime();
+themeToggle.addEventListener('click', () => {
+
+    body.classList.toggle('dark-theme');
     
 
-    setInterval(updateTime, 1000);
+    if (body.classList.contains('dark-theme')) {
+        themeToggle.textContent = 'Mode Clair';
+    } else {
+        themeToggle.textContent = 'Mode Sombre';
+    }
 });
